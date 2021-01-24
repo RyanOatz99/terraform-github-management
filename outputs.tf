cc5ed300b6a39f43a9973689b9520ec4ad27efa3
Chris Frage <git@sh0shin.org>
2021-01-24 09:32:03 +0100

    Added outputs and description

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,23 @@
 # vim: set ft=terraform :
 
-output "github_team_ids" {
-  value = { for team in github_team.this : team.name => team.id }
+# github_team
+output "team_ids" {
+  description = "The IDs of the created teams."
+  value       = { for team in github_team.this : team.name => team.id }
+}
+
+# github_repository
+output "repo_git_clone_urls" {
+  description = "URLs that can be provided to `git clone` to clone the repository anonymously via the git protocol."
+  value       = { for repo in github_repository.this : repo.name => repo.git_clone_url }
+}
+
+output "repo_http_clone_urls" {
+  description = "URLs that can be provided to `git clone` to clone the repository via HTTPS."
+  value       = { for repo in github_repository.this : repo.name => repo.http_clone_url }
+}
+
+output "repo_ssh_clone_urls" {
+  description = "URLs that can be provided to `git clone` to clone the repository via SSH."
+  value       = { for repo in github_repository.this : repo.name => repo.ssh_clone_url }
 }
